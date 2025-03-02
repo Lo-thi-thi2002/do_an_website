204cd83e8e6cad65d74566e7a4926dacfc3a7853
lo-thi-thi2002 <lothithi@gail.com>
2025-03-02 22:50:04 +0700

    init project

---
+++ scripts.js
@@ -0,0 +1,182 @@
+
+        // Danh sách sản phẩm
+        const products = {
+            // thêm ảnh giày nam
+            "giày nam": [
+                { name: "Giày Sneaker Nam", price: "1,200,000đ", image: "hinh_anh/giay_nam/giaynam_1.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_2.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_3.jpg" },
+                { name: "Giày Sneaker Nam", price: "800,000đ", image: "hinh_anh/giay_nam/giaynam_4.jpg" },
+                { name: "Giày Da Nam", price: "1,000,000đ", image: "hinh_anh/giay_nam/giaynam_5.jpg" },
+                { name: "Giày Sneaker Nam", price: "900,000đ", image: "hinh_anh/giay_nam/giaynam_6.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_7.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_8.jpg" },
+                { name: "Giày Da Nam", price: "1,500,000đ", image: "hinh_anh/giay_nam/giaynam_9.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_10.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_11.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_12.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_13.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_14.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_15.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_16.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_17.jpg" },
+                { name: "Giày Sneaker Nam", price: "700,000đ", image: "hinh_anh/giay_nam/giaynam_18.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_19.jpg" },
+                { name: "Giày Sneaker Nam", price: "500,000đ", image: "hinh_anh/giay_nam/giaynam_20.jpg" },
+            ],
+            // thêm ảnh giày nữ
+            "giày nữ": [
+                { name: "1. Giày sneaker nữ", price: "450,000đ", image: "hinh_anh/giay_nu/giaynu_1.jpg" },
+                { name: "2. Giày nữ cute", price: "400,000đ", image: "hinh_anh/giay_nu/giaynu_2.jpg" },
+                { name: "3. Giày Cao Gót Đen", price: "450,000đ", image: "hinh_anh/giay_nu/giaynu_3.jpg" },
+                { name: "4. Giày Búp Bê", price: "350,000đ", image: "hinh_anh/giay_nu/giaynu_4.jpg" },
+                { name: "5. Giày thể thao nữ", price: "550,000đ", image: "hinh_anh/giay_nu/giaynu_5.jpg" },
+                { name: "6. Giày nữ trắng", price: "800,000đ", image: "hinh_anh/giay_nu/giaynu_6.jpg" },
+                { name: "7. Giày thể thao trắng", price: "900,000đ", image: "hinh_anh/giay_nu/giaynu_7.jpg" },
+                { name: "8. Giày Cao Gót Trắng", price: "1,200,000đ", image: "hinh_anh/giay_nu/giaynu_8.jpg" },
+                { name: "9. Giày ánh bạch kim", price: "600,000đ", image: "hinh_anh/giay_nu/giaynu_9.jpg" },
+                { name: "10. Giày hồng trắng xám", price: "750,000đ", image: "hinh_anh/giay_nu/giaynu_10.jpg" },
+                { name: "11. Giày hồng cá tính", price: "700,000đ", image: "hinh_anh/giay_nu/giaynu_11.jpg" },
+                { name: "12. Giày hình mèo", price: "1,050,000đ", image: "hinh_anh/giay_nu/giaynu_12.jpg" },
+                { name: "13. Giày thể thao nữ hồng nhạt", price: "500,000đ", image: "hinh_anh/giay_nu/giaynu_13.jpg" },
+                { name: "14. Giày thể thao nữ trắng sọc đen", price: "600,000đ", image: "hinh_anh/giay_nu/giaynu_14.jpg" },
+                { name: "15. Giày thể thao nữ trắng tinh", price: "850,000đ", image: "hinh_anh/giay_nu/giaynu_15.jpg" },
+                { name: "16. Giày Cao Gót Trắng", price: "950,000đ", image: "hinh_anh/giay_nu/giaynu_16.jpg" },
+                { name: "17. Giày búp bê trắng", price: "700,000đ", image: "hinh_anh/giay_nu/giaynu_17.jpg" },
+                { name: "18. Giày Cao Gót Bạch Kim", price: "1,500,000đ", image: "hinh_anh/giay_nu/giaynu_18.jpg" },
+                { name: "19. Giày Cao Gót Đen Vàng", price: "600,000đ", image: "hinh_anh/giay_nu/giaynu_19.jpg" },
+                { name: "20. Giày Búp Bê Đặc Biệt", price: "800,000đ", image: "hinh_anh/giay_nu/giaynu_20.jpg" },
+                { name: "21. Giày Sneaker Nữ Đen", price: "500,000đ", image: "hinh_anh/giay_nu/giaynu_1.jpg" },
+                { name: "22. Giày Hồng Cute", price: "450,000đ", image: "hinh_anh/giay_nu/giaynu_2.jpg" },
+            ],
+            // thêm ảnh giày bé trai
+            "giày bé trai": [
+                { name: "Giày phong cách thể thao ", price: "500,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_1.jpg" },
+                { name: "Giày thể thao bé trai", price: "350,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_2.jpg" },
+                { name: "Giày Thể Thao Bé Trai  ", price: "500,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_3.jpg" },
+                { name: "Giày thể thao màu đen trắng", price: "450,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_4.jpg" },
+                { name: "Giày thể thao màu loang ", price: "650,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_5.jpg" },
+                { name: "Giày bé trai màu đỏ đô", price: "450,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_6.jpg" },
+                { name: "Giày kiểu màu trắng xám", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_7.jpg" },
+                { name: "Giày kiểu màu đen", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_8.jpg" },
+                { name: "Giày Thể Thao màu ghi", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_9.jpg" },
+                { name: "Giày thể thao màu xanh trắng", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_10.jpg" },
+                { name: "Giày kiểu màu xanh than", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_11.jpg" },
+                { name: "Giày kiểu màu xanh đen", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_12.jpg" },
+                { name: "Giày Thể Thao màu trắng", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_13.jpg" },
+                { name: "Giày thể thao màu trăng đen", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_14.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_15.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_16.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_17.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_18.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_19.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_20.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_21.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_22.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_23.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_24.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_25.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_26.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_27.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_28.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_29.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_30.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_31.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_32.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_33.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_34.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_35.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_36.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_37.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_38.jpg" },
+                { name: "Giày Thể Thao Bé Trai", price: "350,000đ", image:"hinh_anh/giay_be_trai/giaybetrai_39.jpg" },
+                { name: "Giày Sandal Bé Trai", price: "250,000đ", image: "hinh_anh/giay_be_trai/giaybetrai_40.jpg" },
+            ],
+            // thêm ảnh giày bé gái
+            "giày bé gái": [
+                { name: "1. Giày Thỏ Trắng", price: "350,000đ", image: "hinh_anh/giay_be_gai/giaybegai_1.jpg" },
+                { name: "2. Giày Sandal Bé Gái Trắng", price: "400,000đ", image: "hinh_anh/giay_be_gai/giaybegai_2.jpg" },
+                { name: "3. Giày 2 con mèo", price: "400,000đ", image: "hinh_anh/giay_be_gai/giaybegai_3.jpg" },
+                { name: "4. Giày Sandal Bé Gái", price: "280,000đ", image: "hinh_anh/giay_be_gai/giaybegai_.jpg" },
+                { name: "5. Giày Công Chúa Đính Vàng", price: "850,000đ", image: "hinh_anh/giay_be_gai/giaybegai_5.jpg" },
+                { name: "6. Giày Sandal Bé Gái Thắt Cổ Chân", price: "300,000đ", image: "hinh_anh/giay_be_gai/giaybegai_6.jpg" },
+                { name: "7. Giày Công Chúa Thắt Nơ Trắng", price: "450,000đ", image: "hinh_anh/giay_be_gai/giaybegai_7.jpg" },
+                { name: "8. Giày Sandal Bé Gái Có Đèn", price: "600,000đ", image: "hinh_anh/giay_be_gai/giaybegai_8.jpg" },
+                { name: "9. Giày Công Chúa Hồng", price: "400,000đ", image: "hinh_anh/giay_be_gai/giaybegai_9.jpg" },
+                { name: "10. Giày Búp Bê Gắn Ngôi Sao", price: "550,000đ", image: "hinh_anh/giay_be_gai/giaybegai_10.jpg" },
+                { name: "11. Giày Công Chúa Thắt Nơ Màu Da", price: "400,000đ", image: "hinh_anh/giay_be_gai/giaybegai_11.jpg" },
+                { name: "12. Giày Búp Bê Gắn Bông", price: "650,000đ", image: "hinh_anh/giay_be_gai/giaybegai_12.jpg" },
+                { name: "13. Giày Búp Bê Hình Gấu Bông", price: "300,000đ", image: "hinh_anh/giay_be_gai/giaybegai_13.jpg" },
+                { name: "14. Giày Công Chúa Băng Giá", price: "500,000đ", image: "hinh_anh/giay_be_gai/giaybegai_14.jpg" },
+                { name: "15. Giày Công Chúa Bạch Kim", price: "900,000đ", image: "hinh_anh/giay_be_gai/giaybegai_15.jpg" },
+                { name: "16. Giày Công Chúa Phủ Lớp Bạc", price: "700,000đ", image: "hinh_anh/giay_be_gai/giaybegai_16.jpg" },
+                { name: "17. Giày Sandal Bé Gái Có Đèn 3 Màu", price: "650,000đ", image: "hinh_anh/giay_be_gai/giaybegai_17.jpg" },
+                { name: "18. Giày Công Chúa Hình Thỏ Trắng", price: "1,000,000đ", image: "hinh_anh/giay_be_gai/giaybegai_18.jpg" },
+                { name: "19. Giày Công Chúa Hồng Bít Cổ Chân", price: "750,000đ", image: "hinh_anh/giay_be_gai/giaybegai_19.jpg" },
+                { name: "20. Giày Sandal Bé Gái Trắng", price: "500,000đ", image: "hinh_anh/giay_be_gai/giaybegai_20.jpg" },
+            ]
+        };
+
+        function showProducts(category) {
+            const productGallery = document.getElementById("product-gallery");
+            const categoryTitle = document.getElementById("category-title");
+
+            productGallery.innerHTML = ""; // Xóa thư viện cũ
+            categoryTitle.textContent = "Danh mục: " + category.replace(" ", " ").toUpperCase();
+
+            if (products[category]) {
+                products[category].forEach(product => {
+                    productGallery.innerHTML += `
+                        <div class="gallery-item" onclick="showLightbox('${product.image}')">
+                            <img src="${product.image}" alt="${product.name}">
+                            <div class="info">
+                                <p>${product.name}</p>
+                                <p>${product.price}</p>
+                            </div>
+                        </div>
+                    `;
+                });
+            } else {
+                productGallery.innerHTML = "<p>Không có sản phẩm nào!</p>";
+            }
+        }
+
+        // Hiển thị ảnh lớn khi click vào sản phẩm
+        function showLightbox(imageSrc) {
+            document.getElementById("lightbox-img").src = imageSrc;
+            document.getElementById("lightbox").style.display = "flex";
+        }
+
+        // Đóng lightbox khi click ra ngoài
+        document.getElementById("lightbox").addEventListener("click", function (e) {
+            if (e.target === this) {
+                this.style.display = "none";
+            }
+        });
+        // chạy sline ảnh
+        const images = [
+            "hinh_anh/about/about_1.jpg",
+            "hinh_anh/about/about_2.jpg",
+            "hinh_anh/about/about_3.jpg",
+            "hinh_anh/about/about_4.jpg",
+            "hinh_anh/about/about_5.jpg",
+            "hinh_anh/about/about_6.jpg",
+            "hinh_anh/about/about_7.jpg",
+          ];
+          
+          let index = 0;
+          function changeImage() {
+              index = (index + 1) % images.length;
+              document.getElementById("slideshow").src = images[index];
+          }
+  
+          setInterval(changeImage, 3000); // Thay đổi ảnh mỗi 3 giây
+           // Lấy tên người dùng từ localStorage
+        //    hiện thị xin chào ở đầu trang
+        const username = localStorage.getItem("username");
+        // Nếu tên người dùng tồn tại, hiển thị thông điệp chào mừng
+        if (username) {
+            document.getElementById("user").innerText = username;
+            document.getElementById("welcomeMessage").style.display = "block";
+        }
+        
